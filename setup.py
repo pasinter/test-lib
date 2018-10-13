3217c737249263612ddc9f7eb94d7bbe22d93f0e
Ken Golovin <ken.golovin@gmail.com>
2018-10-13 11:22:48 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='common',
+    packages=['common'],
+    description='Common libs',
+    version='1.0',
+    url='https://github.com/pasinter/test-lib/tree/master',
+    author='pasinter',
+    keywords=['common']
+)
